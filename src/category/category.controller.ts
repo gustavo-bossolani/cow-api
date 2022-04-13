a41fb5f2195ef5019a464be6201c33cfde570bef
gustavo-bossolani <gustavo.wyd@outlook.com>
2022-04-12 21:27:50 -0300

    implements create and read for category

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { CategoryService } from './category.service';
+import { Body, Controller, Get, Post, Query } from '@nestjs/common';
+import { FilterCategoryDto } from './dto/filter-category.dto';
+import { Category } from './entity/category.entity';
 
 @Controller('category')
-export class CategoryController {}
+export class CategoryController {
+  constructor(private categoryService: CategoryService) {}
+
+  @Post()
+  createCategory(@Body('name') name: string): void {
+    return this.categoryService.create(name);
+  }
+
+  @Get()
+  getCategory(@Query() filter: FilterCategoryDto): Category {
+    return this.categoryService.getBy(filter);
+  }
+
+  @Get('/all')
+  getAll(): Category[] {
+    return this.categoryService.getAll();
+  }
+}
--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -1,25 +1,51 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
-
+import {
+  BadRequestException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { DefineError } from 'src/shared/models/define-error.model';
-
+import { FilterCategoryDto } from './dto/filter-category.dto';
 import { Category } from './entity/category.entity';
 
 @Injectable()
 export class CategoryService {
   private categories: Category[] = [];
 
-  getBy(name: string) {
-    const found = this.categories.find((cat) => cat.name === name);
-    if (found) {
-      return found;
+  getBy(filter: FilterCategoryDto): Category {
+    const { id, name } = filter;
+
+    let category: Category = null;
+
+    if (!Object.keys(filter).length) {
+      throw new BadRequestException(
+        new DefineError('Isert values to search category.', 400),
+      );
     }
-    throw new UnauthorizedException(
-      new DefineError('Category already Exists', 401),
-    );
+
+    if (id) {
+      category = this.categories.find((cat) => cat.id === id);
+    }
+    if (name) {
+      category = this.categories.find(
+        (cat) => cat.name.toLocaleUpperCase() === name.toLocaleUpperCase(),
+      );
+    }
+
+    return category;
   }
 
-  create(name: string) {
-    this.getBy(name);
+  getAll(): Category[] {
+    return this.categories;
+  }
+
+  create(name: string): void {
+    const category = this.getBy({ name: name.toUpperCase() });
+
+    if (category) {
+      throw new UnauthorizedException(
+        new DefineError('Category already exists.', 401),
+      );
+    }
 
     this.categories.push({
       id: new Date().getMilliseconds().toString(),
