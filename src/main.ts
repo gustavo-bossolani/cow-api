79ea58c0c4da22d7f38f06d69abe10b8db90d628
gustavo-bossolani <gustavo.wyd@outlook.com>
2022-04-19 19:26:17 -0300

    implements logs on application init

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const logger = new Logger();
   const app = await NestFactory.create(AppModule);
+
   app.useGlobalPipes(new ValidationPipe());
+
   await app.listen(3000);
+  logger.log(`Application listening on ${3000}`);
 }
 bootstrap();
