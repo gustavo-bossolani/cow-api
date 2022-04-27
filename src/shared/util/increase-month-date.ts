f9c670aa680ba167a90b568b09e9876aae497f8d
gustavo-bossolani <gustavo.wyd@outlook.com>
2022-04-27 11:23:45 -0300

    implements else return for increase-month util

--- src/shared/util/increase-month-date.ts
+++ src/shared/util/increase-month-date.ts
@@ -1,8 +1,20 @@
+const options: Intl.DateTimeFormatOptions = {
+  day: 'numeric',
+  month: 'numeric',
+  year: 'numeric',
+};
+
+const language = 'pt-br';
+
 const increaseMonth = (quantity: number): string => {
   const date = new Date();
-  date.setMonth(date.getMonth() + quantity);
 
-  return date.toLocaleDateString();
+  if (!quantity) {
+    return new Date().toLocaleDateString(language, options);
+  }
+
+  date.setMonth(date.getMonth() + quantity);
+  return date.toLocaleDateString(language, options);
 };
 
 export { increaseMonth };
