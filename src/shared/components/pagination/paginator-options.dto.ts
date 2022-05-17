5e96fb1b0cc3ad8e45987c9864bda4deadd136ec
gustavo-bossolani <gustavo.wyd@outlook.com>
2022-05-17 19:14:20 -0300

    create swagger for Pagination component

--- src/shared/components/pagination/paginator-options.dto.ts
+++ src/shared/components/pagination/paginator-options.dto.ts
@@ -1,9 +1,22 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsDefined } from 'class-validator';
 
 class PaginatorOptionsDto {
+  @ApiProperty({
+    example: 2,
+    nullable: false,
+    required: true,
+    description: 'Set the current page',
+  })
   @IsDefined()
   page: number;
 
+  @ApiProperty({
+    example: 10,
+    nullable: false,
+    required: true,
+    description: 'Limit of items per page',
+  })
   @IsDefined()
   limit: number;
 }
