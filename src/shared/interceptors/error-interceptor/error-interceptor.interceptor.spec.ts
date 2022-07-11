6e9a8f32e228d1b7dfbd860bd4ef28a2f1efbf46
gustavo-bossolani <gustavo.wyd@outlook.com>
2022-07-11 12:01:05 -0300

    creates global error interceptor

---
+++ src/shared/interceptors/error-interceptor/error-interceptor.interceptor.spec.ts
@@ -0,0 +1,7 @@
+import { ErrorInterceptorInterceptor } from './error-interceptor.interceptor';
+
+describe('ErrorInterceptorInterceptor', () => {
+  it('should be defined', () => {
+    expect(new ErrorInterceptorInterceptor()).toBeDefined();
+  });
+});
---
+++ src/shared/interceptors/error-interceptor/error-interceptor.interceptor.ts
@@ -0,0 +1,35 @@
+import { DefineError } from 'src/shared/models/define-error.model';
+import {
+  CallHandler,
+  ExecutionContext,
+  HttpException,
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+  NestInterceptor,
+} from '@nestjs/common';
+import { catchError, Observable, throwError } from 'rxjs';
+
+@Injectable()
+export class ErrorInterceptorInterceptor implements NestInterceptor {
+  private logger = new Logger('Error interceptor');
+
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    const dev = process.env.ENV === 'dev';
+
+    return next.handle().pipe(
+      catchError((error: HttpException) => {
+        this.logger.error(`Message ${error.message}`);
+
+        if (dev) this.logger.error(`Stack ${error.stack}`);
+
+        return throwError(
+          () =>
+            new InternalServerErrorException(
+              new DefineError('Uncatched error', 500),
+            ),
+        );
+      }),
+    );
+  }
+}
