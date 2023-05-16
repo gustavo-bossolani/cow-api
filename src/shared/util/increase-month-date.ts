70144a26acb41cb88eb05040de40c54ad89f2592
gustavo-bossolani <gustavo.wyd@outlook.com>
2023-05-16 17:53:43 -0300

    fix increaseMonth method

--- src/shared/util/increase-month-date.ts
+++ src/shared/util/increase-month-date.ts
@@ -6,13 +6,20 @@
  * @example increaseMonth('2022-01-15', 4): '2022-05-15'
  */
 const increaseMonth = (startDate: string, quantity: number): string => {
-  const date = new Date(startDate); // TODO: revisar datas com dias a mais
+  if (quantity > 1) {
+    const [year, month, day] = startDate.split('-');
 
-  if (quantity) {
-    date.setMonth(date.getMonth() + quantity);
+    const date = new Date(
+      // Obs: é diminuido duas vezes a quantidade de meses por conta das seguintes regras:
+      // 1 - A contagem de meses no js começa em 0. Ex: Jan = 0, Fev = 1,...
+      // 2 - A primeira parcela é considerada no mês corrente
+      Date.UTC(Number(year), Number(month) - 2 + quantity, Number(day)),
+    );
+
+    return date.toISOString().split('T')[0];
   }
 
-  return date.toISOString().split('T')[0];
+  return startDate;
 };
 
 export { increaseMonth };
