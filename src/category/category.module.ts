265fc43e512d276c6ad6bf4093f4612ab7e49069
gustavo-bossolani <gustavo.wyd@outlook.com>
2023-07-31 10:30:40 -0300

    implements auth module on category module

--- src/category/category.module.ts
+++ src/category/category.module.ts
@@ -1,6 +1,7 @@
-import { AuthModule } from './../auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
+
+import { AuthModule } from './../auth/auth.module';
 
 import { CategoryRepository } from 'src/category/repositories/category.repository';
 
@@ -8,6 +9,7 @@ import { CategoryService } from './category.service';
 import { CategoryController } from './category.controller';
 
 import { Category } from 'src/category/entity/category.entity';
+
 @Module({
   imports: [
     TypeOrmModule.forFeature([CategoryRepository, Category]),
